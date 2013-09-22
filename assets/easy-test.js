c05b45b57a01926ce8057b2fa40671ad5a113bf7
Andrei-Robert Rusu <rusuandreirobert@gmail.com>
2013-09-22 18:15:24 +0300

    Fix

--- assets/easy-test.js
+++ assets/easy-test.js
@@ -187,10 +187,10 @@ if(typeof EasyTest == "undefined") {
                     '</p>';
                   jQuery.each(question.options, function(option, optionScore){
             html += '<div class="option-container" style="' + objectInstance.fetchInlineCSSByDecorationInformation('question-answer-option-container') + '">' +
-                      '<label for="option-' + i + '-' + optionScore + '"' +
+                      '<label for="' + this.internalNamespace + '-' + i + '-' + optionScore + '"' +
                              'style="' + objectInstance.fetchInlineCSSByDecorationInformation('question-answer-option-label') + '"' +
                       '>' + option + '</label>' +
-                      '<input id="option-' + i + '-' + optionScore + '" ' +
+                      '<input id="' + this.internalNamespace + '-' + i + '-' + optionScore + '" ' +
                              'style="' + objectInstance.fetchInlineCSSByDecorationInformation('question-answer-option-radio') + '"' +
                              'type="radio" ' +
                              'name="question_answer[' + i + ']" ' +
